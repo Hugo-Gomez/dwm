56eab1c4f3169fa5ae8c251e03fd46f69826982b
Hugo-Gomez <hugo.gomez9795@gmail.com>
2021-01-27 23:03:59 +0100

    new colors, shortcuts, rules...

--- config.h
+++ config.h
@@ -9,26 +9,31 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar       = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar        = 12;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "FontAwesome:pixelsize=16:antialias=true:autohint=true", "monospace:size=9" };
+static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char norm_fg[]         = "#b0caec";
+static const char norm_bg[]         = "#222d32";
+static const char sel_fg[]          = "#eec58b";
+static const char sel_bg[]          = "#2a353d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/* [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },*/
+	[SchemeNorm] = { norm_fg, norm_bg, norm_bg },
+	[SchemeSel]  = { sel_fg, sel_bg, "#6D92A0" },
 };
 
 /* tagging */
-/*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
 /* HOME :  */
 /* WEB :  */
 /* CODE :  */
 /* TERM :  */
-static const char *tags[] = {"1 ", "2 ", "3 ", "4 ", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"1 ", "2 ", "3 ", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -37,9 +42,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     switchtotag  isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 1,       1,           0,           -1 },
-	{ "code",     NULL,       NULL,       1 << 2,       1,           0,           -1 },
-	{ "st",       NULL,       NULL,       1 << 3,       1,           0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 0,       1,           0,           -1 },
+	{ "code",     NULL,       NULL,       1 << 1,       1,           0,           -1 },
+	{ "st",       NULL,       NULL,       1 << 2,       1,           0,           -1 },
 };
 
 /* layout(s) */
@@ -67,10 +72,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockcmd[] = { "lock", NULL };
 static const char *rangercmd[] = { "st", "-e", "ranger", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -116,6 +122,7 @@ static Key keys[] = {
 	/* Lock screen */
 	{ MODKEY|ShiftMask,		XK_l,	spawn,		    {.v = lockcmd } },	
 	{ MODKEY,			XK_r,	spawn,		    {.v = rangercmd } },	
+	{ MODKEY|ShiftMask,		XK_b,	spawn,		    {.v = browsercmd } },	
 };
 
 /* button definitions */

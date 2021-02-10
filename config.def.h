b1bacef2e6165c5a8ec95a459836c6bb7f56fafd
Hugo-Gomez <hugo.gomez9795@gmail.com>
2021-02-10 23:59:39 +0100

    tweaks

--- config.def.h
+++ config.def.h
@@ -2,8 +2,8 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 5;       /* gap pixel between windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 3;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 	/* [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },*/
 	[SchemeNorm] = { norm_fg, norm_bg, norm_bg },
-	[SchemeSel]  = { sel_fg, sel_bg, "#6D92A0" },
+	[SchemeSel]  = { sel_fg, sel_bg, sel_fg }, /*#6D92A0*/
 };
 
 /* tagging */
@@ -50,7 +50,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
